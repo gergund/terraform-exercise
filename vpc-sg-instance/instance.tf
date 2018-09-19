6c1f7e8750bbbe702fd7f16712c8d75567f1e959
Denys Gergun <gergund@gmail.com>
2018-09-19 12:19:46 +0300

    Added SG rules and instance in private network

--- vpc-sg-instance/instance.tf
+++ vpc-sg-instance/instance.tf
@@ -6,8 +6,24 @@ resource "aws_instance" "bastion" {
   subnet_id = "${aws_subnet.main-public-1a.id}"
 
   # the security group
-  vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
+  vpc_security_group_ids = ["${aws_security_group.SG-Bastion.id}"]
 
   # the public SSH key
   key_name = "${aws_key_pair.mykeypair.key_name}"
 }
+
+resource "aws_instance" "web1" {
+  ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
+  instance_type = "t2.micro"
+
+  # the VPC subnet
+  subnet_id = "${aws_subnet.main-private-1a.id}"
+
+  # the security group
+  vpc_security_group_ids = ["${aws_security_group.SG-Web.id}"]
+
+  # the public SSH key
+  key_name = "${aws_key_pair.mykeypair.key_name}"
+}
+
+
--- vpc-sg-instance/securitygroup.tf
+++ vpc-sg-instance/securitygroup.tf
@@ -1,21 +1,66 @@
-resource "aws_security_group" "allow-ssh" {
-  vpc_id = "${aws_vpc.main.id}"
-  name = "allow-ssh"
-  description = "security group that allows ssh and all egress traffic"
-  egress {
-      from_port = 0
-      to_port = 0
-      protocol = "-1"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
+#Security Group for Bastion
+resource "aws_security_group" "SG-Bastion" {
 
-  ingress {
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  } 
+  vpc_id = "${aws_vpc.main.id}"
+  name = "SG-Bastion"
+
 tags {
-    Name = "allow-ssh"
+    Name = "SG-Bastion"
   }
+}
+
+resource "aws_security_group_rule" "Bastion-ingress_ssh" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.SG-Bastion.id}"
+}
+
+resource "aws_security_group_rule" "Bastion-egress_all" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.SG-Bastion.id}"
+}
+
+#Security Group for Web
+resource "aws_security_group" "SG-Web" {
+
+  vpc_id = "${aws_vpc.main.id}"
+  name = "SG-Web"
+
+tags {
+    Name = "SG-Web"
+  }
+}
+
+resource "aws_security_group_rule" "Web-ingress_ssh" {
+  type = "ingress"
+  from_port = 22
+  to_port = 22
+  protocol = "tcp"
+  source_security_group_id = "${aws_security_group.SG-Bastion.id}"
+  security_group_id = "${aws_security_group.SG-Web.id}"
+}
+
+resource "aws_security_group_rule" "Web-ingress_http" {
+  type = "ingress"
+  from_port = 80
+  to_port = 80
+  protocol = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.SG-Web.id}"
+}
+
+resource "aws_security_group_rule" "Web-egress_all" {
+  type = "egress"
+  from_port = 0
+  to_port = 0
+  protocol = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.SG-Web.id}"
 }
